04b838160fffd6b62f18d60cefe9feb65d2801ac
Arne Schlüter <arne@schlueter.is>
2019-02-18 20:54:10 +0100

    Add react-movable

--- src/main/reagent_movable/core.cljs
+++ src/main/reagent_movable/core.cljs
@@ -1,15 +1,23 @@
 (ns reagent-movable.core
-  (:require [reagent.core :as r]))
-
-(defn app []
-  [:div
-   [:h1 "Hello world"]
-   [:p "If you can read this it means this is working."]])
-
-(defn ^:export mount-into [dom-elem]
-  (r/render-component [app] dom-elem))
-
-;; helpers
+  (:require [reagent.core :as r]
+            ["react-movable" :refer [List]]))
 
 (defn reload! []
   (.. js/window -location reload))
+
+(defonce state (r/atom (vec (map #(str "Item " %) (range 1 11)))))
+
+(defn movable []
+  (let [items @state]
+    [:ul
+     (for [[idx item] (map-indexed vector items)]
+       ^{:key idx} [:li item])]))
+
+(defn app []
+  [:div
+   [:h1 "Movable list below"]
+   [movable]])
+
+(defn ^:dev/after-load start []
+  (println "Mounting component...")
+  (r/render [app] (.getElementById js/document "app")))
